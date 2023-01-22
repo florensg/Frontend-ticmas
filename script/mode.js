f6e7ccfffa1f2c7e2328cb2ad8754ac3b5eae01f
florensg <floo.g1096@gmail.com>
2023-01-22 15:07:53 -0300

    funcionan los botones del cambio de modo oscuro

--- script/mode.js
+++ script/mode.js
@@ -1,9 +1,11 @@
-document.getElementById("boton").addEventListener('click',() => {
-    document.body.classList.toggle("dark")
+document.getElementById('boton').addEventListener('click',() => {
     if (document.getElementById('estilo').getAttribute('href') == 'css/light.css') {
         document.getElementById('estilo').setAttribute('href', 'css/dark.css');
+        document.getElementById('boton').classList.toggle('bi-brightness-high-fill');
     }else{
-        document.getElementById('estilo').setAttribute('href', 'css/light.css');
+        document.getElementById('boton').classList.toggle('bi-brightness-high-fill');
+        document.getElementById('estilo').setAttribute('href', 'css/light.css')
     }
 
+   
 })
