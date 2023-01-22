6ade7227dd832173f516c3b4d3e3e1b934626f7f
florensg <floo.g1096@gmail.com>
2023-01-22 12:54:55 -0300

    agregado transiciones al background-color de cada modo

--- script/mode.js
+++ script/mode.js
@@ -1,3 +1,9 @@
-document.getElementById("boton").onclick = function (params) {
-    
-}
+document.getElementById("boton").addEventListener('click',() => {
+    document.body.classList.toggle("dark")
+    if (document.getElementById('estilo').getAttribute('href') == 'css/light.css') {
+        document.getElementById('estilo').setAttribute('href', 'css/dark.css');
+    }else{
+        document.getElementById('estilo').setAttribute('href', 'css/light.css');
+    }
+
+})
